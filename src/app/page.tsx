4a7a634e168408d1e5adea7071d8bc7da723bd14
black_spider <80927775+Arthur-geek@users.noreply.github.com>
2026-08-20 19:03:09 +0530

    analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import WhyApply from "@/components/sections/WhyApply";
 import Proof from "@/components/sections/Proof";
 import Partners from "@/components/sections/Partners";
 import Apply from "@/components/sections/Apply";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
       <Proof />
       <Partners />
       <Apply />
+      <Analytics />
     </main>
   );
 }
